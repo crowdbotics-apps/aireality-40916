7003b103ca22d1a331d8ad584da67911d6e4582b
Crowdbotics <team@crowdbotics.com>
2023-05-23 05:12:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aireality-40916",
+  url: "https://aireality-40916.botics.co",
+  api: "https://aireality-40916.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
